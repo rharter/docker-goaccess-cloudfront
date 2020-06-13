58508bc8c3ada0c408f58756c7f0f57f1354b656
Ryan Harter <ryanjharter@gmail.com>
2020-06-13 16:43:30 -0500

    Adds support for post actions, including pruning processed logs.

--- root/app/sync.sh
+++ root/app/sync.sh
@@ -3,7 +3,7 @@
 echo "INFO: Starting sync.sh PID $$ $(date)"
 
 if [ -z "$BUCKET" ]; then
-	echo "Missing BUCKET environment variable."
+	echo "ERROR: Missing BUCKET environment variable."
 	exit 1
 fi
 
@@ -13,5 +13,40 @@ aws s3 sync s3://${BUCKET} /logs
 
 echo "INFO: Generating analytics html from logs."
 zcat /logs/*.gz | goaccess --log-format CLOUDFRONT -a -o /config/html/${HTML_FILENAME:-index}.html -
+
+case "$POST_ACTION" in
+	"" )
+		;;
+
+	"prune" )
+		PRUNE=1
+		;;
+
+	* )
+		if [ -x "${POST_ACTION}" ]; then
+			echo "INFO: Executing post action script: ${POST_ACTION}"
+			sh -c "${POST_ACTION}" "/config/html/${HTML_FILENAME:-index}.html"
+		elif [ -x "/config/${POST_ACTOIN}" ]; then
+			echo "INFO: Executing post action script: ${POST_ACTION}"
+			sh -c "/config/${POST_ACTION}" "/config/html/${HTML_FILENAME:-index}.html"
+		else
+			echo "INFO: Executing post action: ${POST_ACTION}"
+			eval "${POST_ACTION}"
+		fi
+		;;
+
+esac
+
+if [ -n "$PRUNE" ];then
+	let log_count=$(ls -l /logs | wc -l | awk '{$1=$1};1')-1
+	let prune_count=log_count-1
+	
+	echo "INFO: Pruning ${prune_count} old logs."
+
+	newest_log_filename=$(ls -tp /logs | head -n 1)
+	find /logs/ -type f -not -name "${newest_log_filename}" -delete
+
+	aws s3 sync /logs s3://${BUCKET} --delete
+fi
 
 echo "INFO: Completed sync.sh PID $$ $(date)"
